caa4beade26ccbb123843edadad204943e6402ec
Markus Marihart <herp@derp.com>
2015-04-06 14:36:46 +0200

    Finished all levels. Improved memory and cpu performance of code. Improved structure of code (more subroutines for better readability).

--- auction_bidding.c
+++ auction_bidding.c
@@ -9,101 +9,166 @@
 #define MIN_BID 1
 #define BID_STEP 1
 #define BIDDERS 2
+#define TRUE 1
 
+#define LEVEL 4
 
-void createOutputArray(char output[], int size, char nextText[], int * index) {
+char * createOutputArray(char * output, char nextText[], int * index) {
     int x = 0;
+    char * temp;
     while(nextText[x] != '\0') {
-        output[*index+x] = nextText[x];
+
         x++;
+    }
+    temp = (char *) malloc(sizeof(char) * (*index+x+1));
+    memcpy(temp, output, *index);
+    free(output);
+    output = temp;
+    for(int i = 0; i < x; i++) {
+        output[*index+i] = nextText[i];
     }
     output[*index+x] = ',';
     *index = *index+x+1;
+    return output;
+}
+
+
+int inverseNumbers(char * numberArray, int count) {
+    int helper = 0;
+    for(int x = 0; x < count/2; x++) {
+        helper = numberArray[x];
+        numberArray[x] = numberArray[count-1-x];
+        numberArray[count-1-x] = helper;
+    }
+}
+
+
+int intToChar(char * array, int number, int inverse) {
+    int count = 0;
+    for (; number != 0; number = number / 10) {
+        array[count++] = (number % 10) + '0';
+    }
+    array[count] = '\0';
+    if(inverse && number >= 10) {
+        inverseNumbers(array, count);
+    }
+}
+
+
+int getNextBid(char * input, int * offset) {
+    int count = 0;
+    int bid = 0;
+    while(input[count+*offset] != ',' && input[count+*offset] != '\0') {
+        count++;
+    }
+    for(int x = 0; x < count; x++) {
+        bid += (input[*offset+x]-'0') * pow(10, count-x-1);
+    }
+    if(input[count+*offset] == '\0') {
+        *offset = *offset + count;
+    } else {
+        *offset = *offset + count + 1;
+    }
+    return bid;
+}
+
+int getNextBidder(char * input, int *offset, char * act_bidder) {
+    int count = 0;
+    while(input[count+*offset] != ',' && input[count+*offset] != '\0') {
+        count++;
+    }
+
+    act_bidder = (char*) realloc(act_bidder, sizeof(char)*count);
+    if(act_bidder == NULL) {
+        printf("Error reallocating memory!\n");
+    }
+    for(int x = 0; x < count; x++) {
+        act_bidder[x] = input[*offset+x];
+    }
+    act_bidder[count] = '\0';
+    *offset = *offset + count + 1;
 }
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
         return 0;
     }
-    int input[] = {1,'A',5,'B',10,'A',8,'A',17,'B',17};
-    int length = 0;
-    int bid_count = 0;
-    int bidder_count = 0;
     unsigned int price = 0;
-    char highest_bidder[30];
-    char act_bidder[30];
+    char * highest_bidder;
+    char * act_bidder;
     unsigned int bidder_max = 0;
 
-    int bids[10];
-    char bidders[5];
     int i = 0;
     int sum = 0;
 
-    int temp = 0;
-    char helperArray[200];
-    char output[300];
-    int oIndex = 0;
+    int offset = 0;
+    int old_offset = 0;
+    int getBidder = 0;
+    int size;
     int write = 0;
-    int instantBuy = 0;
-    int auctionRunning = 1;
-    int y = 0;
+
+    #if LEVEL>=3
+        char * helperArray;
+        char * output;
+        int oIndex = 0;
+        output = (char *) malloc(sizeof(char)*1);
+        int addSize = 0;
+    #endif
+    #if LEVEL >= 4
+        int instantBuy = 0;
+        int auctionRunning = 1;
+    #endif
+
+
+    act_bidder = (char*) malloc(sizeof(char)*1);
+    highest_bidder = (char*) malloc(sizeof(char)*1);
+    if(act_bidder == NULL || highest_bidder == NULL) {
+        printf("Error allocating memory!");
+        return 0;
+    }
 
     //get start bid
-    while(argv[1][length] >= '0' && argv[1][length] <= '9') {
-        length++;
-    }
+    old_offset = offset;
+    price = getNextBid(argv[1], &offset);
 
-    for(int x = 0; x < length; x++) {
-        price += (argv[1][x]-'0') * pow(10, length-x-1);
-    }
+    #if LEVEL >= 3
+        size = offset - old_offset - 1;
+        helperArray = (char *) malloc(sizeof(char) * size);
+        output = createOutputArray(output, "-", &oIndex);
+        intToChar(helperArray, price, TRUE);
+        output = createOutputArray(output, helperArray, &oIndex);
+    #endif
 
-
-    createOutputArray(output, 100, "-\0", &oIndex);
-    temp = 0;
-    for(int num = price; num != 0; num=num/10) {
-        helperArray[temp++] = (num%10)+'0';
-    }
-    helperArray[temp] = '\0';
-    for(int x = 0; x < temp/2; x++) {
-        int helper = helperArray[x];
-        helperArray[x] = helperArray[temp-1-x];
-        helperArray[temp-1-x] = helper;
-    }
-    createOutputArray(output, 100, helperArray, &oIndex);
-    length++;
-    y = length;
     //get instant buy price
-    while(argv[1][length] >= '0' && argv[1][length] <= '9') {
-        length++;
-    }
-    for(; y < length; y++) {
-        instantBuy += (argv[1][y]-'0') * pow(10, length-y-1);
-    }
+    #if LEVEL >= 4
+        instantBuy = getNextBid(argv[1], &offset);
+    #endif
 
-    while(argv[1][length] != '\0' && auctionRunning) {
+    getBidder = 1;
+    while(argv[1][offset] != '\0') {
+        #if LEVEL >= 4
+            if(auctionRunning == 0) {
+                break;
+            }
+        #endif
+        if(getBidder) {
+            old_offset = offset;
+            getNextBidder(argv[1], &offset, act_bidder);
+            size = offset-old_offset-1;
+            getBidder = 0;
+            write = 0;
+        } else {
+            sum = getNextBid(argv[1], &offset);
+            getBidder = 1;
 
-
-        if(argv[1][length] >= 'A' && argv[1][length] <= 'Z' || argv[1][length] >= 'a' && argv[1][length] <= 'z') {
-            i = length;
-            while(argv[1][i] != ',') {
-                act_bidder[i-length] = (char) argv[1][i];
-                i++;
-            }
-            act_bidder[i-length] = '\0';
-            length = ++i;
-            while(argv[1][i] >= '0' && argv[1][i] <= '9') {
-                i++;
-            }
-            for(int x = length; x < i; x++) {
-                sum += (argv[1][x]-'0') * pow(10, i-x-1);
-            }
             if(sum > bidder_max) {
                 write = 1;
-                if(memcmp(highest_bidder, act_bidder, 20) == 0) {
+                if(memcmp(highest_bidder, act_bidder, size) == 0) {
                     write = 0;
                 } else if(bidder_max != 0) {
                     price = bidder_max + 1;
                 }
+                highest_bidder = (char*) realloc(highest_bidder, sizeof(char)*size);
                 memcpy(highest_bidder, act_bidder, 20);
 
                 bidder_max = sum;
@@ -114,52 +179,33 @@ int main(int argc, char* argv[]) {
             } else {
                 price = bidder_max;
                 write=1;
-
             }
+
+            #if LEVEL >= 4
             if(price >= instantBuy && instantBuy != 0) {
                 price = instantBuy;
                 auctionRunning = 0;
             }
-            if(write != 0) {
-                createOutputArray(output, 100, highest_bidder, &oIndex);
-
-                temp = 0;
-                for (int num = price; num != 0; num = num / 10) {
-                    helperArray[temp++] = (num % 10) + '0';
-                }
-                helperArray[temp] = '\0';
-                for(int x = 0; x < temp/2; x++) {
-                    int helper = helperArray[x];
-                    helperArray[x] = helperArray[temp-1-x];
-                    helperArray[temp-1-x] = helper;
-                }
-                createOutputArray(output, 100, helperArray, &oIndex);
-            }
+            #endif
 
             sum = 0;
         }
 
-        length++;
-    }
-    output[oIndex-1] = '\0';
-    /*
-    for(int i = 1; i < length; i++) {
-        if(input[i] >= 'A' && input[i] <= 'Z') {
-
-            if(input[i+1] > bidder_max) {
-                price = bidder_max + 1;
-                highest_bidder = (char) input[i];
-                bidder_max = input[i+1];
-            } else if(input[i+1] == bidder_max) {
-                price = bidder_max;
-            } else {
-                price = input[i+1] + 1;
-            }
-            i++;
+        #if LEVEL >= 3
+        if(write != 0) {
+            output = createOutputArray(output, highest_bidder, &oIndex);
+            helperArray = (char*) realloc(helperArray, sizeof(char)*size);
+            intToChar(helperArray, price, TRUE);
+            output = createOutputArray(output, helperArray, &oIndex);
         }
+        #endif
     }
-*/
-   // printf("%s,%d\n", highest_bidder, price);
-    printf("%s\n", output);
+    #if LEVEL >= 3
+        output[oIndex-1] = '\0';
+        printf("%s\n", output);
+    #else
+        printf("%s,%d\n", highest_bidder, price);
+    #endif
+
     return 0;
 }
